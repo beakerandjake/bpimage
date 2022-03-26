d17f2587bd8ba5bce8acc026fa4cead0750d6f9c
beakerandjake <>
2022-03-25 21:16:35 -0700

    first pass simple blur

--- bpimage/bpimage.py
+++ bpimage/bpimage.py
@@ -1,5 +1,42 @@
 
 import numpy as np
 
+
+blur_kernal = np.ones((3,3)) * 1/9
+'''
+    1    [1,1,1],
+    - *  [1,1,1],
+    9    [1,1,1]  
+'''
+
 def blur(img: np.ndarray):
-    return img
+    dest = np.zeros_like(img)
+    h,w = img.shape[:2]
+
+    for y in range(h):
+        for x in range(w):
+            window = img[max(0,y-1):min(y+2,h), max(0,x-1):min(x+2,w)]
+            result = np.sum(window, axis=(0,1)) / 9
+            dest[y,x] = result
+
+            # print('x',x,'y',y,'pixel', img[y,x])
+            # # print(f'slice: {max(0,y-1)}:{min(y+2,h)},{max(0,x-1)}:{min(x+2,w)}')
+            # # print(window)
+            # print(window)
+            # print('==')
+            # print(np.sum(window, axis=-1))
+            # print('-----')
+            # print()
+    
+    return dest
+            
+
+def _convolve(img,kernel):
+    dest = np.zeros_like(img)
+    h,w = img.shape[:2]
+
+    for y in range(h):
+        for x in range(w):
+            pass
+
+    return dest

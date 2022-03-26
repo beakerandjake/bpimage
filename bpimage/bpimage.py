f882fe9b85d3377bbdef853ff6fa15f4d7730510
beakerandjake <>
2022-03-25 18:17:28 -0700

    scaffold blur

---
+++ bpimage/bpimage.py
@@ -0,0 +1,5 @@
+
+import numpy as np
+
+def blur(img: np.ndarray):
+    return img
--- bpimage/main.py
+++ bpimage/main.py
@@ -3,12 +3,14 @@
 import sys
 from argparse import ArgumentParser
 import io_utils
+import bpimage
 
 def parse_args():
     parser = ArgumentParser(description="CLI for bpimage library")
     parser.add_argument('source', help="source image file")
     parser.add_argument('-o', '--output', help='destination image file')
     parser.add_argument('-d','--debug', action='store_true', help='creates a temporary image and displays using the default image viewer')
+    parser.add_argument('-b','--blur', action='store_true', help='blurs the source image')
 
     if len(sys.argv) < 2:
         parser.print_help(sys.stderr)
@@ -18,6 +20,9 @@ def parse_args():
 
 def process_img(args):
     img = io_utils.open(args.source)
+
+    if(args.blur):
+        img = bpimage.blur(img)
 
     if(args.output):
         io_utils.save(img, args.output)
@@ -30,7 +35,7 @@ def main():
 
     try:
         process_img(args)
-    except (io_utils.ImageOpenError, io_utils.ImageSaveError) as e: 
+    except (io_utils.ImageOpenError, io_utils.ImageSaveError, io_utils.ImageShowError) as e: 
         return str(e)
 
 if __name__ == '__main__':

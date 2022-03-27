4a6a6fb800da835c178d09f3587e1d3cd34b0295
beakerandjake <>
2022-03-27 13:48:19 -0700

    minimal speedup of simple convolve

--- bpimage/bpimage.py
+++ bpimage/bpimage.py
@@ -1,7 +1,4 @@
-
-from calendar import c
 import numpy as np
-from cprof import cprof
 
 # boxblur = np.full((9,9), 1/81, dtype=np.float32)
 # gaussian = np.array([[1,2,1],[2,4,2],[1,2,1]], dtype=np.float32) / 16
@@ -10,7 +7,6 @@ from cprof import cprof
 # top_sobel = np.array([[1,2,1],[0,0,0],[-1,-2,-1]], dtype=np.float32)
 # sharpen = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]], dtype=np.float32)
 
-@cprof('profram.prof')
 def boxblur(img:np.ndarray, radius:int=1) -> np.ndarray:  
     if radius < 1:
         return img
@@ -18,36 +14,40 @@ def boxblur(img:np.ndarray, radius:int=1) -> np.ndarray:
     size = (radius*2)+1 
     boxkern = np.full(np.full(2,size), 1/size**2, dtype=np.float32)
 
-    return _clip(_convolve(img.astype(np.float32), boxkern))
+    return _clip(_convolve_skip(img.astype(np.float32), boxkern))
 
-def _convolve(img,kernel):
-    dest = np.empty_like(img)
+def _convolve_simple(img,kern):
+    dest = np.zeros_like(img)
     h,w = img.shape[:2]
-    kh,kw = kernel.shape
+    kh,kw = kern.shape
     kpad = kw // 2
-    kern = kernel[::-1,::-1]
     
     for y in range(h):
         for x in range(w):
-            # print(f'img: {(y,x)} -> {img[y,x]}')
-            acc = np.zeros(3, dtype=np.float32)
             for ky in range(kh):
                 for kx in range(kw):
-                    oy = y+ky-kpad
-                    ox = x+kx-kpad
-                    wy = _clamp(oy,h-1)
-                    wx = _clamp(ox,w-1)
-                    pix = kern[ky,kx] * img[wy,wx]
-                    acc += pix
-                    # print(f'kernel: ({(ky,kx)}) = {kern[ky,kx]} -> offset: {oy,ox} -> window: {(wy,wx)} -> pmod: {pix} -> acc: {acc}')
-            # print(f'new pixel: {acc}')
-            dest[y,x] = acc
-            # print('---')
-
+                    dest[y,x] += kern[ky,kx] * img[_clamp(y+ky-kpad,h-1),_clamp(x+kx-kpad,w-1)]
+                    # print(f'kernel: ({(ky,kx)}) = {kern[ky,kx]} -> offset: {oy,ox} -> window: {(wy,wx)} -> pmod: {pix} -> acc: {acc}'
     return dest
 
 def _clamp(n,max_n):
     return max(0,min(n, max_n))
+
+def _convolve_skip(img,kern):
+    """Convolve but ignore the boundaries"""
+    dest = np.zeros_like(img)
+    h,w = img.shape[:2]
+    kh,kw = kern.shape
+    kpad = kw // 2
+    
+    for y in range(kpad, h-kpad):
+        for x in range(kpad, w-kpad):
+            for ky in range(kh):
+                for kx in range(kw):
+                    dest[y,x] += kern[ky,kx] * img[y+ky-kpad,x+kx-kpad]
+                    # print(f'kernel: ({(ky,kx)}) = {kern[ky,kx]} -> offset: {oy,ox} -> window: {(wy,wx)} -> pmod: {pix} -> acc: {acc}'
+    return dest
+    
 
 def _clip(img):
     return np.clip(img,0,255).astype(np.uint8)

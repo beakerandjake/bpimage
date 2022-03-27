0420398ace8ec153c2ce25725ff02e2c35b5479e
beakerandjake <>
2022-03-27 08:51:17 -0700

    wip simple convolve

--- bpimage/bpimage.py
+++ bpimage/bpimage.py
@@ -1,44 +1,45 @@
 
+from calendar import c
 from cgi import print_exception
 import numpy as np
 
 
-blur_kernal = np.full((3,3), 1/9)
-'''
-    1    [1,1,1],
-    - *  [1,1,1],
-    9    [1,1,1]  
-'''
-
-g_kernel = np.array([[1,2,1],[2,4,2],[1,2,1]]) / 16
+boxblur = np.full((3,3), 1/9)
+gaussian = np.array([[1,2,1],[2,4,2],[1,2,1]]) / 16
+emboss = np.array([[-2,-1,0],[-1,1,1],[0,1,2]])
 
 def blur(img: np.ndarray):
-    return _convolve(img, g_kernel)            
+    return _convolve(img, gaussian)            
 
 def _convolve(img,kernel):
     dest = np.empty_like(img)
     h,w = img.shape[:2]
     kh,kw = kernel.shape
+    kpad = kw // 2
+    kern = kernel[::-1,::-1]
+    
+    # _print_pixels(img)
+    # print(kernel)
+    # print(kh,kw)
+    # print(kpad)
 
-    print(kernel.shape)
-    print(h,w)
-
-    # for y in range(h):
-    #     for x in range(w):
-    #         acc = 0
-            
-    #         for ky in range(3):
-    #             for kx in range(3):
-    #                 acc += kernel[ky,kx] * window[ky,kx]
-
-    #         # r = np.sum(window * kernel, axis=(0,1))
-    #         # _print_pixels(window)
-    #         # print(acc)
-    #         # _print_pixels(window * kernel)
-    #         dest[y-1,x-1] = acc
-    #         # return dest
+    for y in range(h):
+        for x in range(w):
+            # print(f'img: {(y,x)}')
+            acc = 0
+            # iterate over kernal
+            for ky in range(kh):
+                for kx in range(kw):
+                    # acc += kernel[kh-ky-1,kw-kx-1] * img[_clamp(ky-kpad+y,kh-1),_clamp(kx-kpad+x,kw-1)]
+                    acc += kernel[kh-ky-1,kw-kx-1] * img[_clamp(y+ky-kpad,kh-1),_clamp(x+kx-kpad,kw-1)]
+                    # print(f'kernel: ({kh-ky-1},{kw-kx-1}) -> window: {(ky,kx)} -> offset: {(ky-kpad+y, kx-kpad+x)} -> img: {(_clamp(y+ky-kpad,kh-1),_clamp(x+kx-kpad,kw-1))}')
+            dest[y,x] = acc
+            # print('---')
 
     return dest
+
+def _clamp(n,max_n):
+    return max(0,min(n, max_n))
 
 def _pad(img):
     ret = np.zeros((img.shape[0]+2, img.shape[1]+2, img.shape[2]), dtype=img.dtype)

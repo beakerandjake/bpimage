5c7f962660fbebdceb1a89061b2029b42706df26
beakerandjake <>
2022-03-27 17:15:08 -0700

    adding some filters and arguments

--- bpimage/bpimage.py
+++ bpimage/bpimage.py
@@ -1,29 +1,57 @@
 import numpy as np
 
-# boxblur = np.full((9,9), 1/81, dtype=np.float32)
-# gaussian = np.array([[1,2,1],[2,4,2],[1,2,1]], dtype=np.float32) / 16
-# gaussian_5 = np.array([[1,4,6,4,1],[4,16,24,16,4],[6,24,36,24,6],[4,16,24,16,4],[1,4,6,4,1]], dtype=np.float32) / 256
-emboss = np.array([[-2,-1,0],[-1,1,1],[0,1,2]], dtype=np.float32)
-edge = np.array([[0,1,0],[1,-4,1],[0,1,0]], dtype=np.float32)
-edge2 = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], dtype=np.float32)
-sharpen = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]], dtype=np.float32)
-# top_sobel = np.array([[1,2,1],[0,0,0],[-1,-2,-1]], dtype=np.float32)
-# sharpen = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]], dtype=np.float32)
+def edge_detect(img:np.ndarray) -> np.ndarray:
+    kern = np.array([[-1,-1,-1],
+                     [-1, 8,-1],
+                     [-1,-1,-1]], dtype=np.float32)
+    return _clip(_convolve_padded(img.astype(np.float32),_kern3d(kern)))
+
+def sharpen(img:np.ndarray) -> np.ndarray:
+    kern = np.array([[1, 1, 1],
+                     [1,-7, 1],
+                     [1, 1, 1]], dtype=np.float32)
+    return _clip(_convolve_padded(img.astype(np.float32),_kern3d(kern)))
+
+def emboss(img:np.ndarray) -> np.ndarray:
+    kern = np.array([[2, 0, 0],
+                     [0,-1, 0],
+                     [0, 0,-1]], dtype=np.float32)
+    return _clip(_convolve_padded(img.astype(np.float32),_kern3d(kern),bias=128.0))
 
 def boxblur(img:np.ndarray, radius:int=1) -> np.ndarray:  
     if radius < 1:
         return img
-    
+        
     size = (radius*2)+1 
     boxkern = np.full(np.full(2,size), 1/size**2, dtype=np.float32)
 
     return _clip(_convolve_padded(img.astype(np.float32),_kern3d(boxkern)))
 
+def _convolve_padded(img, kern,bias=0.0):
+    """convolve that extends the image boundaries via padding"""
+    dest = np.empty_like(img)
+    krad = kern.shape[0] // 2
+    kh,kw = kern.shape[:2]
+    pad = np.pad(img,((krad,krad),(krad,krad),(0,0)),'edge')
+
+    for y in range(img.shape[0]):
+        for x in range(img.shape[1]):
+            dest[y,x] = np.sum(pad[y:y+kh,x:x+kw]*kern, axis=(0,1)) + bias
+
+    return dest
+    
+def _clip(img):
+    # return (img * 255.0).astype(np.uint8)
+    return np.clip(img,0,255).astype(np.uint8)
+
+def _kern3d(kern1d):
+    return np.repeat(kern1d[...,None],3,axis=-1)
+
 def _convolve_slow(img,kern):
     """naive convolve operation using loops, very slow."""
     dest = np.zeros_like(img)
     h,w = img.shape[:2]
-    kh,kw = kern.shape
+    kh,kw = kern.shape[:2]
     kpad = kw // 2
     
     for y in range(h):
@@ -48,25 +76,3 @@ def _convolve_skip_boundary(img,kern):
             dest[y,x] = np.sum(img[y-krad:y+krad+1,x-krad:x+krad+1]*kern, axis=(0,1))
             
     return dest
-
-def _convolve_padded(img, kern):
-    """convolve that extends the image boundaries via padding"""
-    dest = np.empty_like(img)
-    krad = kern.shape[0] // 2
-    kh,kw = kern.shape[:2]
-    pad = np.pad(img,((krad,krad),(krad,krad),(0,0)),'edge')
-
-    for y in range(img.shape[0]):
-        for x in range(img.shape[1]):
-            dest[y,x] = np.sum(pad[y:y+kh,x:x+kw]*kern, axis=(0,1))
-
-    return dest
-
-
-    
-def _clip(img):
-    # return (img * 255.0).astype(np.uint8)
-    return np.clip(img,0,255).astype(np.uint8)
-
-def _kern3d(kern1d):
-    return np.repeat(kern1d[...,None],3,axis=-1)
--- bpimage/main.py
+++ bpimage/main.py
@@ -4,12 +4,23 @@ from argparse import ArgumentParser
 import io_utils
 import bpimage
 
+# todo 
+# allow multiple filters, and execute them in a loop piping output of one to input of next
+# figure out best way to wrap / clamp values for uint8
+# output file with before/after for debigging
+# convolve in c and call from python
+# pass arguments to filters
+
 def parse_args():
     parser = ArgumentParser(description="CLI for bpimage library")
     parser.add_argument('source', help="source image file")
     parser.add_argument('-o', '--output', help='destination image file')
     parser.add_argument('-d','--debug', action='store_true', help='creates a temporary image and displays using the default image viewer')
+    # filters
     parser.add_argument('-b','--blur', action='store_true', help='blurs the source image')
+    parser.add_argument('-s','--sharpen', action='store_true', help='sharpen the source image')
+    parser.add_argument('-ed','--edgedetect', action='store_true', help='edge detect the source image')
+    parser.add_argument('-e','--emboss', action='store_true', help='emboss the source image')
 
     if len(sys.argv) < 2:
         parser.print_help(sys.stderr)
@@ -22,6 +33,15 @@ def process_img(args):
 
     if(args.blur):
         img = bpimage.boxblur(img,1)
+    
+    if(args.sharpen):
+        img = bpimage.sharpen(img)
+    
+    if(args.edgedetect):
+        img = bpimage.edge_detect(img)
+    
+    if(args.emboss):
+        img = bpimage.emboss(img)
 
     if(args.output):
         io_utils.save(img, args.output)

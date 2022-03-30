014a20b30b79c5f8f72e9f3cd6508fd0a742ab60
beakerandjake <>
2022-03-30 16:20:12 -0700

    wip convolve

--- bpimage/convolve.c
+++ bpimage/convolve.c
@@ -6,26 +6,33 @@
 
 void fn(float *img, size_t *img_strides, size_t *img_size, float *kern, size_t *kern_strides, size_t *kern_size)
 {
-    size_t height = img_size[0];
-    size_t width = img_size[1];
-    // convert strides from byte steps to "pointer increments"
-    size_t s0 = img_strides[0] / sizeof(float);
-    size_t s1 = img_strides[1] / sizeof(float);
-    size_t s2 = img_strides[2] / sizeof(float);
+    size_t height, width, s0, s1, s2;
+    height = img_size[0]; width = img_size[1];
+    // convert imgage strides from byte steps to pointer increments
+    s0 = img_strides[0] / sizeof(float);
+    s1 = img_strides[1] / sizeof(float);
+    s2 = img_strides[2] / sizeof(float);
+
+    size_t kern_height, kern_width, ks0, ks1;
+    kern_height = kern_size[0]; kern_width = kern_size[1];
+    // convert kernel strides from byte steps to pointer increments. 
+    ks0 = kern_strides[0] / sizeof(float);
+    ks1 = kern_strides[1] / sizeof(float);
+
+    // iterate every pixel of the image
+
+        // iterate every pixel of the kernel 
+
+            // sum the pixels 
+
 
     printf("img shape: (%zd,%zd)\n", height, width);
     printf("strides raw: (%zd,%zd,%zd)\n", img_strides[0], img_strides[1], img_strides[2]);
     printf("strides: (%zd,%zd,%zd)\n", s0, s1, s2);
 
-    size_t kern_height = kern_size[0];
-    size_t kern_width = kern_size[1];
-    size_t ks0 = kern_strides[0] / sizeof(float);
-    size_t ks1 = kern_strides[1] / sizeof(float);
-
     printf("kern shape: (%zd,%zd)\n", kern_height, kern_width);
     printf("kern strides raw: (%zd,%zd,%zd)\n", kern_strides[0], kern_strides[1], kern_strides[2]);
     printf("kern strides: (%zd,%zd)\n", ks0, ks1);
-
 
     // // zero out red and green
 

44aedeaafc4aed0b1773aa98869230049f40d60f
beakerandjake <>
2022-04-07 11:36:59 -0700

    use memcpy to copy pixel data, update comments

--- bpimage/affine.c
+++ bpimage/affine.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+
+// The memory size of a single RGB pixel (composed of 3 unsigned chars for RGB)
+const size_t PIXEL_SIZE = 3 * sizeof(unsigned char);
 
 void affine_transform(unsigned char *img, size_t *img_shape, size_t *img_strides, float *inv_transform, unsigned char *dest, size_t *dest_shape, size_t *dest_strides)
 {
@@ -12,42 +16,44 @@ void affine_transform(unsigned char *img, size_t *img_shape, size_t *img_strides
     size_t ds0 = dest_strides[0];
     size_t ds1 = dest_strides[1];
 
+    // x and y scale
     float a0 = inv_transform[0];
     float a1 = inv_transform[1];
+    // x and y shear
     float b0 = inv_transform[3];
     float b1 = inv_transform[4];
+    // x and y offset
     float c0 = inv_transform[6];
     float c1 = inv_transform[7];
 
-    size_t x1, y1, x, y, src_offset, dest_offset;
+    size_t x1, y1, x, y;
 
+    // iterate each pixel in the destination image.
     for (y1 = 0; y1 < dest_height; y1++)
     {
         for (x1 = 0; x1 < dest_width; x1++)
         {
+            // calculate the location of the source pixel by applying the inverse transformation matrix.
             x = x1 * a0 + y1 * b0 + c0;
             y = x1 * a1 + y1 * b1 + c1;
 
+            // skip any locations which fall outside of the source image. 
             if ((x < 0 || x >= img_width) || (y < 0 || y >= img_height))
             {
                 continue;
             }
 
-            dest_offset = ds0 * y1 + ds1 * x1;
-            src_offset = s0 * y + s1 * x;
-
-            dest[dest_offset] = img[src_offset];
-            dest[++dest_offset] = img[++src_offset];
-            dest[++dest_offset] = img[++src_offset];
+            // copy the source pixel to the destination pixel.
+            memcpy(dest + ds0 * y1 + ds1 * x1, img + s0 * y + s1 * x, PIXEL_SIZE);
         }
     }
 }
 
-    // printf("transform\n");
-    // printf("[%f,%f,%f]\n", a0, a1, a2);
-    // printf("[%f,%f,%f]\n", b0, b1, b2);
-    // printf("[%f,%f,%f]\n", c0, c1, 0);
-    // printf("5/2 = {%ld}\n", (size_t)(.5 * 5));
+// printf("transform\n");
+// printf("[%f,%f,%f]\n", a0, a1, a2);
+// printf("[%f,%f,%f]\n", b0, b1, b2);
+// printf("[%f,%f,%f]\n", c0, c1, 0);
+// printf("5/2 = {%ld}\n", (size_t)(.5 * 5));
 
-    // printf("img strides (%ld,%ld)", s0,s1);
-    // printf("img shape (%ld,%ld)", img_height,img_width);
+// printf("img strides (%ld,%ld)", s0,s1);
+// printf("img shape (%ld,%ld)", img_height,img_width);
